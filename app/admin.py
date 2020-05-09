ea5f9dcd68f20201f0d20fa862834dac087d4949
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2020-05-09 09:16:18 -0300

    remove unused import in admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import FotoServico, AdicionalDeServico, Servico, CategoriaDeServico, CarrinhoDeServicos, ItemServico, \
+from app.models import FotoServico, AdicionalDeServico, Servico, CarrinhoDeServicos, ItemServico, \
     AdicionalEscolhido
 
 
